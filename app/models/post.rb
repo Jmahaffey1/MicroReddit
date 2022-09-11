9804df4acfb31c4cd52e79861e44fdc87863bca6
Jacob <mahaffeyjake1@gmail.com>
2022-09-11 12:45:06 -0500

    added comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,5 @@ class Post < ApplicationRecord
     validates :title, presence: true, uniqueness: { case_sensitive: false }, length: {maximum: 40}
     validates :body, presence: true, length: {maximum: 300}
     belongs_to :user, foreign_key: "user_id"
-    
-    
-    
+    has_many :comments
 end
